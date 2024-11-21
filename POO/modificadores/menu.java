a03c25f7dcf08691a3d5df760ef2bf8f4e755f3b
Guilherme-Kaynam <guilhermekaynam2006@gmail.com>
2024-11-21 17:41:34 -0300

    Trabalhando com Modificadores pt 1

---
+++ POO/modificadores/menu.java
@@ -0,0 +1,13 @@
+package modificadores;
+
+public class menu {
+
+	public static void main(String[] args) {
+		
+		modificador modificador = new modificador();
+		
+		modificador.teste();
+
+	}
+
+}
---
+++ POO/modificadores/modificador.java
@@ -0,0 +1,11 @@
+package modificadores;
+
+public class modificador {
+	
+	private String nome;
+	
+	public void teste() {
+		System.out.println("Testado");
+	}
+
+}
