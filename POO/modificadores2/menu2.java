146b69677fe23354e22a9a0234aeff247f0d5537
Guilherme-Kaynam <guilhermekaynam2006@gmail.com>
2024-11-21 17:42:06 -0300

    Trabalhando com Modificadores pt 2

---
+++ POO/modificadores2/menu2.java
@@ -0,0 +1,15 @@
+package modificadores2;
+
+import modificadores.modificador;
+
+public class menu2 {
+
+	public static void main(String[] args) {
+		
+		modificador modificador = new modificador();
+		
+		modificador.teste();
+
+	}
+
+}
