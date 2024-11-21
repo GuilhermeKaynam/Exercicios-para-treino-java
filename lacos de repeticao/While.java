39605708843080e3527c9dcab1a68a3310d05ff5
Guilherme-Kaynam <guilhermekaynam2006@gmail.com>
2024-11-21 17:00:42 -0300

    Exercicio com Laços de Repetição

---
+++ lacos de repeticao/While.java
@@ -0,0 +1,37 @@
+package lacos.repeticao;
+
+import java.util.Scanner;
+
+public class While {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		boolean continua;
+		
+		System.out.println("\n++++++++++++++++MENU++++++++++++++++++");
+		System.out.print("\nDeseja usar nossa calculadora de soma? (s/n): ");
+		continua = scanner.next().equalsIgnoreCase("s");
+		System.out.println("++++++++++++++++++++++++++++++++++");
+		
+		while(continua) {
+			
+			 System.out.println("Digite primeiro valor: ");
+	         int   numero1 = scanner.nextInt();
+
+	            System.out.println("Digite segundo valor: ");
+	          int  numero2 = scanner.nextInt();
+	          
+	          int resul = numero1 - numero2;
+	          System.out.println("O resultado da soma é: " + resul);
+	          
+	  		System.out.print("\nDeseja continuar? (s/n): ");
+	  		continua = scanner.next().equalsIgnoreCase("s");
+	  		System.out.println("Obrigado, até a proxima");
+	  		
+			
+		}
+
+	}
+
+}
